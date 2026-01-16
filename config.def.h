f8202659e7393350a54cef37b12933c1c23e79b4
arvindpz <parvind97@gmail.com>
2026-01-16 20:34:32 +0530

    Add colors to dmenu

--- config.def.h
+++ config.def.h
@@ -7,17 +7,20 @@ static int min_width = 500;                    /* minimum width when centered */
 static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"monospace:size=20"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt      = ">";      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+			/*     fg         bg */
+			/* black,  mistyrose */
+	[SchemeNorm] = { "#000000", "#d9c0bf" },
+
+			/* plum,   whitesmoke */
+	[SchemeSel] = { "#f5f5f5", "#a19ac8" },
+	[SchemeOut] = { "#f5f5f5", "#a19ac8" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 5;
 
 /*
  * Characters not considered part of a word while deleting words
